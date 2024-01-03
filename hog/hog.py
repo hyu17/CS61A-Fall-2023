a15026d5995214f3db683368bf8f053899e997d6
George YU <yumocoding@gmail.com>
2024-01-03 16:44:54 +0800

    Project: Solve Problem 3 in Game Hog
    Implement 'take_turn' function for players to take turns rolling dice.

--- hog/hog.py
+++ hog/hog.py
@@ -53,6 +53,8 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided):
     """Return the points scored on a turn rolling NUM_ROLLS dice when the
     player has PLAYER_SCORE points and the opponent has OPPONENT_SCORE points.
 
+    If num_rolls is zero, use BOAR BRAWL strategy, otherwise use ROLL DICE strategy.
+
     num_rolls:       The number of dice rolls that will be made.
     player_score:    The total score of the current player.
     opponent_score:  The total score of the other player.
@@ -63,7 +65,9 @@ def take_turn(num_rolls, player_score, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return boar_brawl(player_score, opponent_score)
+    return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
